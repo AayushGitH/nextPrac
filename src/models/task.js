6f43a1d4c1dfd27917ffb3250f6b1d10022ebbdb
Aayush <aayushtiwari160@gmail.com>
2024-01-10 19:59:54 +0530

    Added task model

---
+++ src/models/task.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose"
+
+const TaskSchema = new Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    addedDate: {
+        type:Date,
+        default: Date.now(),
+        required: true
+    },
+    status:{
+        type: String,
+        enum: ['pending','completed'],
+        required: true
+    }
+})
+
+// This will create a collection named tasks
+export const Task = mongoose.models.tasks || mongoose.model("tasks", TaskSchema)
